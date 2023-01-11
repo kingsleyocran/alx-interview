b4fdbc381f6fd058cce9538b2fdfb956c346f533
Kingsley Ocran <ocrankingsley@gmail.com>
2023-01-11 16:30:31 +0000

    fix lint warnings

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-'''The minimum operations coding challenge.
-'''
+"""The minimum operations coding challenge.
+"""
 
 
 def minOperations(n):
-    '''Computes the fewest number of operations needed to result
+    """Computes the fewest number of operations needed to result
     in exactly n H characters.
-    '''
+    """
     if not isinstance(n, int):
         return 0
-    
     ops_count = 0
     clipboard = 0
     done = 1
-    
+
     # print('H', end='')
     while done < n:
         if clipboard == 0:
